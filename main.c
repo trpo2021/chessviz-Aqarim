9a2f2cfba4fd679124beae9efc4e5507ddb4e887
Alexey <alexegoshin0403@yandex.ru>
2021-02-26 16:44:55 +0300

    Fix pawns and rename const a <-> suze

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-const int a = 8;
+const int size = 8;
 
 int main()
 {
-    char table[a][a];
+    char table[size][size];
 
     // create table
-    for (int i = 0; i < a; ++i) {
-        for (int j = 0; j < a; ++j) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
             table[i][j] = '0';
         }
     }
 
     // pawns
-    for (int i = 0; i < a; ++i) {
-        table[1][i] = 'P';
-        table[6][i] = 'p';
+    for (int i = 0; i < size; ++i) {
+        table[1][i] = 'p';
+        table[6][i] = 'P';
     }
 
     // otcher
@@ -37,14 +37,14 @@ int main()
     table[7][4] = 'K';
 
     // print table
-    for (int i = 0; i < a; ++i) {
-        printf("%d ", a - i);
-        for (int j = 0; j < a; ++j) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", size - i);
+        for (int j = 0; j < size; ++j) {
             printf("%c ", table[i][j]);
         }
         printf("\n");
     }
-    printf("  a b c d e f g h");
+    printf("  a b c d e f g h\n");
 
     return 0;
 }
