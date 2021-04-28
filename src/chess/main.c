c6e84f889d4a2ffc447d439f1a56b8af1ff46b93
Alexey <alexegoshin0403@yandex.ru>
2021-04-28 15:08:32 +0300

    Edit main.c

--- src/chess/main.c
+++ src/chess/main.c
@@ -1,50 +1,24 @@
+#include <chesslib/board_content.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 const int size = 8;
 
 int main()
 {
-    char table[size][size];
+    char** board = new char*[size];
 
-    // create table
     for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            table[i][j] = '0';
-        }
+        board[i] = new char[size];
     }
 
-    // pawns
-    for (int i = 0; i < size; ++i) {
-        table[1][i] = 'p';
-        table[6][i] = 'P';
-    }
+    board_content(board);
 
-    // otcher
-    table[0][0] = 'r';
-    table[0][7] = 'r';
-    table[7][0] = 'R';
-    table[7][7] = 'R';
-    table[0][1] = 'n';
-    table[0][6] = 'n';
-    table[7][1] = 'N';
-    table[7][6] = 'N';
-    table[0][2] = 'b';
-    table[0][5] = 'b';
-    table[7][2] = 'B';
-    table[7][5] = 'B';
-    table[0][3] = 'q';
-    table[7][3] = 'Q';
-    table[0][4] = 'k';
-    table[7][4] = 'K';
-
-    // print table
     for (int i = 0; i < size; ++i) {
-        printf("%d ", size - i);
-        for (int j = 0; j < size; ++j) {
-            printf("%c ", table[i][j]);
-        }
-        printf("\n");
+        delete board[i];
     }
-    printf("  a b c d e f g h\n");
+    delete board;
+    board = NULL;
 
     return 0;
 }
