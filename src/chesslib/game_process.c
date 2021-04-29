c9f92b213294242d4dfaad3ca5f5f2ca1c521d49
Alexey <alexegoshin0403@yandex.ru>
2021-04-29 10:17:18 +0300

    Create procedure of playing a chess

---
+++ src/chesslib/game_process.c
@@ -0,0 +1,13 @@
+const int size_cords = 8;
+
+/*
+game_process - the procedure of playing a chess
+*/
+
+void game_process(char** board)
+{
+    int coordinates[size_cords];
+    do {
+        board_print(board);
+    } while (true);
+}
