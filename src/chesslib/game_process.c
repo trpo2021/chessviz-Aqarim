07fe3ac0bc2a92d804c866958a2847bbd3d38941
Alexey <alexegoshin0403@yandex.ru>
2021-05-04 09:33:14 +0300

    Add move chess figure

--- src/chesslib/game_process.c
+++ src/chesslib/game_process.c
@@ -1,6 +1,8 @@
 #include "board_print.h"
 #include "check_correct_input.h"
 #include "enter_move.h"
+#include "game_process.h"
+#include "move.h"
 #include <stdio.h>
 
 const int size_cords = 8;
@@ -15,7 +17,9 @@ void game_process(char** board)
     do {
         board_print(board);
         enter_move(coordinates);
-        if (!(check_correct_input(coordinates)))
+        if (check_correct_input(coordinates))
+            move(board, coordinates);
+        else
             printf("Input Error!\n");
     } while (true);
 }
